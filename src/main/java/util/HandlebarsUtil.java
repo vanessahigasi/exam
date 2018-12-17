ff2c6d2b227062b6f2436203ba9701c40a75f333
vanessahigasi <vanessahigasi@gmail.com>
2018-12-17 09:22:58 +0100

    create exam class

---
+++ src/main/java/util/HandlebarsUtil.java
@@ -0,0 +1,26 @@
+package util;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
+import java.io.IOException;
+
+public class HandlebarsUtil {
+
+    private static Handlebars handlebars;
+
+    static {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".hbs");
+        handlebars = new Handlebars(loader);
+    }
+
+    public static Template compile(String templateName) throws IOException {
+
+        return handlebars.compile(templateName);
+    }
+
+}
